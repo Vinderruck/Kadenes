1c00606b8e125944b672a0b5c0547aa18702932e
Vinderruck1 <rodgersmukhonji64@gmail.com>
2024-08-06 22:25:21 +0300

    first commit

---
+++ Kadanes.js
@@ -0,0 +1,15 @@
+function MaxCalculation() {
+    let array = [3, -1, 1, 4, -1];
+    let MaxCurrent = array[0];
+    let MaxGlobal = array[0];
+  
+    for (let i = 1; i < array.length; i++) {
+      MaxCurrent = Math.max(array[i], MaxCurrent + array[i]);
+      MaxGlobal = Math.max(MaxGlobal, MaxCurrent);
+    }
+  
+    console.log(MaxGlobal);
+  }
+  
+  MaxCalculation();
+  
